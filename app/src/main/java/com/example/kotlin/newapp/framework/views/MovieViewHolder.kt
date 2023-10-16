faf11d67ddc2530acb3947823ccd0f38c931e7e7
VON0222 <A01704793@tec.mx>
2023-10-15 23:36:36 -0600

    fix(API): Correct errors and add handlers for API call

--- app/src/main/java/com/example/kotlin/newapp/framework/views/MovieViewHolder.kt
+++ app/src/main/java/com/example/kotlin/newapp/framework/views/MovieViewHolder.kt
@@ -1,11 +1,17 @@
 package com.example.kotlin.mypokedexapp
 
+import android.content.Context
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlin.newapp.data.movie.BaseMovie
 import com.example.kotlin.newapp.databinding.ItemMovieBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MovieViewHolder(private val binding: ItemMovieBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(item: BaseMovie){
         binding.TVName.text = item.title
     }
+
 }
