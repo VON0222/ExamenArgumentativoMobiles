92991e18730792a5812cd2f7482c75faf2bed4a4
VON0222 <A01704793@tec.mx>
2023-10-19 02:04:28 -0600

    chore(MovieListRequirement): Change code for addition of MovieListRequirement

--- app/src/main/java/com/example/kotlin/newapp/framework/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/kotlin/newapp/framework/viewmodel/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.kotlin.newapp.data.network.model.MovieObject
 import com.example.kotlin.newapp.data.MovieRepository
+import com.example.kotlin.newapp.domain.MovieListRequirement
 import com.example.kotlin.newapp.utils.Constants
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -12,11 +13,11 @@ import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
     val movieObjectLiveData = MutableLiveData<MovieObject>()
+    private val movieListRequirement = MovieListRequirement()
 
     fun getMovieList() {
         viewModelScope.launch(Dispatchers.IO) {
-            val movieRepository = MovieRepository()
-            val result: MovieObject? = movieRepository.getMovieList(Constants.language, 1)
+            val result: MovieObject? = movieListRequirement(Constants.language, 1)
             CoroutineScope(Dispatchers.Main).launch {
                 movieObjectLiveData.postValue(result!!)
             }
